0c67151bb5a41107743a378e430cfa90adec3239
Erika <erikaec2100@gmail.com>
2023-07-17 14:28:26 -0700

    Initial Commit

---
+++ erudite-cafe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  erudite-cafe
+//
+//  Created by erika on 7/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
