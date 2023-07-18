c9eb17b01cbebf0442aa6caa5e694b17364e37ca
Erika <erikaec2100@gmail.com>
2023-07-18 14:38:03 -0700

    loading screen, embedding looker studio, 1/2 sign up

--- erudite-cafe/ViewController.swift
+++ erudite-cafe/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import WebKit
+
 
 class ViewController: UIViewController {
 
+    let webView = WKWebView()
+    
+    @IBOutlet weak var webview: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        getData(dataCode: "c02ae9a0-5f8a-4247-ae69-454219763d14/page/q2hWD")
+        
     }
 
-
+    func getData(dataCode:String) {
+        let url = URL(string: "https://lookerstudio.google.com/embed/reporting/\(dataCode)")
+        webview.load(URLRequest(url: url!))
+    }
+    
 }
 
