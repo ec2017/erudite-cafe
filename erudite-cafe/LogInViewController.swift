32e9e40c0d6f82cb5b3bd1f0dac30d00b16134cb
Erika <erikaec2100@gmail.com>
2023-07-21 10:23:48 -0700

    final final project

---
+++ erudite-cafe/LogInViewController.swift
@@ -0,0 +1,48 @@
+//
+//  LogInViewController.swift
+//  erudite-cafe
+//
+//  Created by erika on 7/19/23.
+//
+
+import UIKit
+import Firebase
+
+class LogInViewController: UIViewController {
+
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginClicked(_ sender: Any) {
+        guard let email = emailText.text else { return }
+        guard let password = passwordText.text else { return }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { firebaseResult, error in
+            if let e = error {
+                print(error)
+            }
+            
+            else {
+                self.performSegue(withIdentifier: "gotoDashboard", sender: self)
+            }
+            
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- erudite-cafe/ViewController.swift
+++ erudite-cafe/ViewController.swift
@@ -10,23 +10,30 @@ import WebKit
 
 
 class ViewController: UIViewController {
-
-    let webView = WKWebView()
-    
-    @IBOutlet weak var webview: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        getData(dataCode: "c02ae9a0-5f8a-4247-ae69-454219763d14/page/q2hWD")
-        
+    // Do any additional setup after loading the view.
+
+        getData(dataCode: "f4bfe675-abbd-4d4c-b15f-f96d54401b4d/page/EGTXD")
+
     }
+    
+     
+    
+    
+    
+    let webView = WKWebView()
+
+    @IBOutlet weak var webview: WKWebView!
 
     func getData(dataCode:String) {
         let url = URL(string: "https://lookerstudio.google.com/embed/reporting/\(dataCode)")
         webview.load(URLRequest(url: url!))
     }
-    
-}
 
+
+
+
+
+}
